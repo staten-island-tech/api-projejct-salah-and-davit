570de8480e15d165459efed019ed38ba6d1a35f4
Salahaldeenibrahim <salahaldeenibrahim6@gmail.com>
2023-01-21 16:53:47 -0500

    d

--- app/Flaskr/__init__.py
+++ app/Flaskr/__init__.py
@@ -9,17 +9,23 @@ def index():
 
 @app.route('/pokemon')
 def pokemon():
+    try:
         name = request.args['poke_name']
         poke_data = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}').json()
         poke_types = [t['type']['name'] for t in poke_data['types']]
         poke_abilities = [a['ability']['name'] for a in poke_data['abilities']]
         poke_stats = {s['stat']['name']: s['base_stat'] for s in poke_data['stats']}
-        return render_template('pokemon.html', poke_data=poke_data, poke_types=poke_types, poke_abilities=poke_abilities, poke_stats=poke_stats)
+        return render_template('pokemon.html', poke_data=poke_data, poke_types=poke_types,       poke_abilities=poke_abilities, poke_stats=poke_stats)
+    except:
+        return render_template('error.html')
 
 @app.route('/pokemons')
 def pokemons():
-    poke_list = requests.get('https://pokeapi.co/api/v2/pokemon?limit=5000').json()['results']
-    return render_template('pokemons.html', poke_list=poke_list)
+    try:
+        poke_list = requests.get('https://pokeapi.co/api/v2/pokemon?limit=5000').json()['results']
+        return render_template('pokemons.html', poke_list=poke_list)
+    except:
+        return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
